45a92ec5d1fe5bed763764b2e42e8908e97c7fa8
Sara Elh <saraelhaimer23@gmail.com>
2019-05-16 09:46:10 +0000

    Product Manager Pages Built and Test

--- Shop/Shop.DataAccess.InMemory/ProductRepository.cs
+++ Shop/Shop.DataAccess.InMemory/ProductRepository.cs
@@ -8,7 +8,7 @@ using Shop.Core.Models;
 
 namespace Shop.DataAccess.InMemory
 {
-    class ProductRepository
+    public class ProductRepository
     {
         ObjectCache cache = MemoryCache.Default;
         List<Product> products;
