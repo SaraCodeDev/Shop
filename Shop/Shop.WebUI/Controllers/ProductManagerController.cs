c0b9df361bb3567452e2b57c85ea93cd0b95d90d
Sara Elh <saraelhaimer23@gmail.com>
2019-05-18 17:22:33 +0000

    Added Product Upload Images

--- Shop/Shop.WebUI/Controllers/ProductManagerController.cs
+++ Shop/Shop.WebUI/Controllers/ProductManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -37,7 +38,7 @@ namespace Shop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -45,6 +46,11 @@ namespace Shop.WebUI.Controllers
             }
             else
             {
+                if(file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                }
                 context.Insert(product);
                 context.Commit();
 
@@ -71,7 +77,7 @@ namespace Shop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -84,11 +90,15 @@ namespace Shop.WebUI.Controllers
                 {
                     return View(product);
                 }
-                else
-                {
+                
+                 if (file != null)
+                    {
+                        productToEdit.Image = productToEdit.Id + Path.GetExtension(file.FileName);
+                        file.SaveAs(Server.MapPath("//Content//ProductImages//") + product.Image);
+                    }
                     productToEdit.Category = product.Category;
                     productToEdit.Description = product.Description;
-                    productToEdit.Image = product.Image;
+                    
                     productToEdit.Name = product.Name;
                     productToEdit.Price = product.Price;
 
@@ -96,7 +106,7 @@ namespace Shop.WebUI.Controllers
                     return RedirectToAction("Index");
                 }
             }
-        }
+        
 
         public ActionResult Delete(string Id)
         {
